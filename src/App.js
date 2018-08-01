42ed4d0e400a4ead3b1b9fd3df95370defe76997
liubinbin <lotosbin@gmail.com>
2018-08-01 15:03:03 +0800

    feat: add project page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 class App extends Component {
   render() {
@@ -12,14 +13,14 @@ class App extends Component {
 
                 <ul style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
                     <li><Link to="/">Home</Link></li>
+                    <li><Link to="/projects">Projects</Link></li>
                     <li><Link to="/about">About</Link></li>
-                    <li><Link to="/topics">Topics</Link></li>
                 </ul>
 
                 <hr/>
                 <Route exact path="/" component={Home}/>
+                <Route path="/projects" component={Projects}/>
                 <Route path="/about" component={About}/>
-
             </div>
         </Router>
     );
---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css'
+import logo from "../logo.svg";
+
+const Projects = () => (
+    <div>
+
+    </div>
+);
+export default Projects;
