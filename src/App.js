9094ddb5af60190106defe4bdd4224b1a064159f
liubinbin <lotosbin@gmail.com>
2018-08-14 20:27:35 +0800

    feat: appbar style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
 import './App.css';
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import Home from "./pages/Home";
@@ -11,19 +13,42 @@ import client from "./apollo/client";
 import Projects from "./pages/Projects";
 import Project from "./pages/Project";
 import ProjectGantt from "./pages/ProjectGantt";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import auth from "./auth";
 
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    flex: {},
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
 class App extends Component {
   render() {
-    return (
+      const {classes} = this.props;
+      return (
         <ApolloProvider client={client}>
             <Router>
                 <div className="App">
-                    <ul style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/projects">Projects</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                    </ul>
-                    <hr/>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Typography variant="title" color="inherit" className={classes.flex}>
+                                xRedmine
+                            </Typography>
+                            <ul style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flex: 1}}>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/projects">Projects</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                            </ul>
+                            {auth.isAuthenticated() ? <Button color="inherit" component={Link} to={"/logout"}>Logout</Button> : <Button color="inherit" component={Link} to={"/login"}>Login</Button>}
+                        </Toolbar>
+                    </AppBar>
                     <Route exact path="/" component={Home}/>
                     <PrivateRoute path="/projects" component={Projects}/>
                     <PrivateRoute path="/project/:projectId" component={Project}/>
@@ -38,4 +63,8 @@ class App extends Component {
   }
 }
 
-export default App;
+App.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
