fd31396730afa36afea40b63bb8a98435246a228
lotosbin <lotosbin@gmail.com>
2019-01-28 15:43:57 +0800

    style: clean

--- src/pages/project_home/components/Gantt.js
+++ src/pages/project_home/components/Gantt.js
@@ -1,17 +1,6 @@
 import moment from "moment";
 import React, {Component} from "react";
 import DHXGantt from "./DHXGantt";
-
-const datasample = {
-    data: [
-        {id: 1, text: "Task #1", start_date: "15-04-2017", duration: 3, progress: 0.6},
-        {id: 2, text: "Task #2", start_date: "18-04-2017", duration: 3, progress: 0.4},
-    ],
-    links: [
-        {id: 1, source: 1, target: 2, type: "0"},
-    ],
-};
-
 export interface IGanttIssue {
     id: string;
     subject: string;
